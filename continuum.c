8e5c97b91e958ad372e90417049803c9ba508ddc
Kevin Wells <wellsie1116@gmail.com>
2011-12-01 15:13:53 -0500

    Add stubs for event handling

--- continuum.c
+++ continuum.c
@@ -18,12 +18,80 @@ void configure()
 	gl_configure();
 }
 
+void handle_active_event(SDL_ActiveEvent* event)
+{
+	if (!event->gain)
+	{
+		printf("TODO pause\n");
+	}
+}
+
+void handle_keyboard_event(SDL_KeyboardEvent* event)
+{
+	const char* state;
+	if (event->state == SDL_PRESSED)
+		state = "pressed";
+	else
+		state = "released";
+
+	printf("%c key %s\n", event->keysym.sym, state);
+
+}
+
+void handle_mouse_motion_event(SDL_MouseMotionEvent* event)
+{
+
+}
+
+void handle_mouse_button_event(SDL_MouseButtonEvent* event)
+{
+	const char* button;
+	switch (event->button)
+	{
+		case SDL_BUTTON_LEFT:
+			button = "left";
+			break;
+		case SDL_BUTTON_MIDDLE:
+			button = "middle";
+			break;
+		case SDL_BUTTON_RIGHT:
+			button = "right";
+			break;
+		default:
+			button = "unknown";
+			break;
+	}
+
+	const char* state;
+	if (event->state == SDL_PRESSED)
+		state = "pressed";
+	else
+		state = "released";
+
+	printf("%s button %s at %d, %d\n", button, state, event->x, event->y);
+}
+
 bool handle_event(SDL_Event* event)
 {
 	switch (event->type)
 	{
 		case SDL_QUIT:
 			return true;
+		case SDL_ACTIVEEVENT:
+			handle_active_event(&event->active);
+			break;
+		case SDL_KEYDOWN:
+		case SDL_KEYUP:
+			handle_keyboard_event(&event->key);
+			break;
+		case SDL_MOUSEMOTION:
+			handle_mouse_motion_event(&event->motion);
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+		case SDL_MOUSEBUTTONUP:
+			handle_mouse_button_event(&event->button);
+			break;
+			
 	}
 	return false;
 }
