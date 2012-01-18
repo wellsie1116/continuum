5a441d3e536051931bdf31d55fba80a03d78f2cf
Kevin Wells <wellsie1116@gmail.com>
2012-01-18 11:46:07 -0500

    Update time log script

--- timelog.sh
+++ timelog.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Commits:"
-git log --since="yesterday" "--pretty=format:%s" | grep -E "[0-9]*(\.[0-9]*)h"
+git log --since="last week" "--pretty=format:%s" | grep -E ".*\+([0-9]*(\.[0-9]*)?)h"
 
 echo
 echo -n "Total: "
-git log --since="yesterday" "--pretty=format:%s" | sed -rn "s/.*\+([0-9]*(\.[0-9]*)?)h/\1/ p" | paste -sd+ | bc
+git log --since="last week" "--pretty=format:%s" | sed -rn "s/.*\+([0-9]*(\.[0-9]*)?)h/\1/ p" | paste -sd+ | bc
